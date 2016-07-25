72f8ba48c4946d37a8cc7e1e07795ce373d5a189
lifter035 <lifter035@users.noreply.github.com>
2016-07-25 11:50:15 +0100

    Update propertiesReader.js
    Added options:
    var options = {
      variables: true,
      namespaces: true
    };

--- src/propertiesReader.js
+++ src/propertiesReader.js
@@ -3,7 +3,12 @@
 var sync = require('synchronize');
 var properties = require('properties');
 
+var options = {
+  variables: true,
+  namespaces: true
+};
+
 module.exports = function (source) {
   sync(properties, 'parse');
-  return properties.parse(source);
+  return properties.parse(source, options);
 };
